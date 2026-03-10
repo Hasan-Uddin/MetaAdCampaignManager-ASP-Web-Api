5c8664c0c59693345e9b9b92af29ab475987c118
Hasan Uddin <abcnizamd@gmail.com>
2026-03-10 12:54:54 +0600

    update

--- src/Application/Abstractions/Services/Meta/IMetaApiService.cs
+++ src/Application/Abstractions/Services/Meta/IMetaApiService.cs
@@ -1,6 +1,8 @@
 using Application.Features.Meta.Ads.Get;
 using Application.Features.Meta.AdSets.Get;
 using Application.Features.Meta.Campaigns.Get;
+using Application.Features.Meta.Forms;
+using Application.Features.Meta.Forms.Create;
 using Application.Features.Meta.Leads.Get;
 using SharedKernel;
 
@@ -12,4 +14,8 @@ public interface IMetaApiService
     Task<Result<List<AdSetResponse>>> GetAdSetsAsync(string campaignId, CancellationToken ct = default);
     Task<Result<List<AdResponse>>> GetAdsAsync(string adSetId, CancellationToken ct = default);
     Task<Result<List<LeadResponse>>> GetLeadsAsync(string formId, CancellationToken ct = default);
+    Task<Result<List<FormResponse>>> GetFormsAsync(string pageId, CancellationToken ct = default);
+    Task<Result<string>> CreateFormAsync(CreateFormCommand command, CancellationToken ct = default);
+    Task<Result<FormResponse>> GetFormByIdAsync(string formId, CancellationToken ct = default);
+    Task<Result> DeleteFormAsync(string formId, CancellationToken ct = default);
 }
