604aff7be2dae599b7ae0a3bc9ea05a53047556b
Hasan Uddin <abcnizamd@gmail.com>
2026-03-10 12:54:03 +0600

    init

---
+++ src/Application/Features/Meta/MetaSettings/Create/CreateeMetaSettingsCommand.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Features.Meta.MetaSettings.Create;
+
+public sealed record CreateeMetaSettingsCommand(
+    string AppId,
+    string AppSecret,
+    string UserToken,
+    string WebhookVerifyToken) : ICommand;
