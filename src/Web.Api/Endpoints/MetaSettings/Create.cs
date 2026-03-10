c16aff29b8a0f7afb1f8d365c8718ab9a9adebe8
Hasan Uddin <abcnizamd@gmail.com>
2026-03-10 13:05:24 +0600

    Update

---
+++ src/Web.Api/Endpoints/MetaSettings/Create.cs
@@ -0,0 +1,32 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Features.Meta.MetaSettings.Create;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.MetaSettings;
+
+internal sealed class Create : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("meta/settings/set", async (
+            CreateeMetaSettingsCommand command,
+            ICommandHandler<CreateeMetaSettingsCommand> handler,
+            CancellationToken cancellationToken) =>
+        {
+            Result result = await handler.Handle(command, cancellationToken);
+            return result.Match(Results.NoContent, CustomResults.Problem);
+        }).WithTags(Tags.MetaSettings)
+        .Produces<List<CreateeMetaSettingsCommand>>();
+    }
+}
+
+
+// eg
+//{
+//  "appId": "123456789",
+//  "appSecret": "abc123...",
+//  "userToken": "short_lived_token_from_graph_explorer",
+//  "webhookVerifyToken": "any-random-string-you-choose"
+//}
--- src/Web.Api/Endpoints/Tags.cs
+++ src/Web.Api/Endpoints/Tags.cs
@@ -5,4 +5,10 @@ public static class Tags
     public const string Users = "Users";
     public const string Auth = "Auth";
     public const string Meta = "Meta";
+    public const string Forms = "Forms";
+    public const string AdSets = "AdSets";
+    public const string Cmapaigns = "Cmapaigns";
+    public const string Ads = "Ads";
+    public const string Leads = "Leads";
+    public const string MetaSettings = "MetaSettings";
 }
