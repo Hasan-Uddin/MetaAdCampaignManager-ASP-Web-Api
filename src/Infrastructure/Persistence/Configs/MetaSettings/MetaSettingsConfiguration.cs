c480e9e5c0f16b6b89db2f03991f03da5e267082
Hasan Uddin <abcnizamd@gmail.com>
2026-03-10 13:03:33 +0600

    init

---
+++ src/Infrastructure/Persistence/Configs/MetaSettings/MetaSettingsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.MetaSettings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configs.MetaSettings;
+
+internal sealed class MetaSettingsConfiguration : IEntityTypeConfiguration<MetaSetting>
+{
+    public void Configure(EntityTypeBuilder<MetaSetting> builder)
+    {
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.AppId).HasMaxLength(100);
+        builder.Property(s => s.AppSecret).HasMaxLength(200);
+        builder.Property(s => s.AccessToken).HasMaxLength(500);
+        builder.Property(s => s.PageId).HasMaxLength(50);
+        builder.Property(s => s.AdAccountId).HasMaxLength(50);
+        builder.Property(s => s.WebhookVerifyToken).HasMaxLength(200);
+        builder.Property(s => s.PageAccessToken).HasMaxLength(500);
+    }
+}
---
+++ src/Infrastructure/Persistence/FormQuestions/FormQuestionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.FormQuestions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.FormQuestions;
+
+internal sealed class FormQuestionConfiguration : IEntityTypeConfiguration<FormQuestion>
+{
+    public void Configure(EntityTypeBuilder<FormQuestion> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.Id)
+            .HasMaxLength(70);
+        
+        builder.Property(f => f.Type)
+            .HasMaxLength(50);
+
+        builder.Property(f => f.Label)
+            .HasMaxLength(50);
+    }
+}
