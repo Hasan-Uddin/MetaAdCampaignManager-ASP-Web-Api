8daee5fe87a460397c725681ac243e1fed8214ff
Hasan Uddin <abcnizamd@gmail.com>
2026-03-12 20:15:43 +0600

    migration 🩹

---
+++ src/Infrastructure/Migrations/20260311174618_Migration_3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "app_id",
+                schema: "public",
+                table: "meta_settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "app_secret",
+                schema: "public",
+                table: "meta_settings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "app_id",
+                schema: "public",
+                table: "meta_settings");
+
+            migrationBuilder.DropColumn(
+                name: "app_secret",
+                schema: "public",
+                table: "meta_settings");
+        }
+    }
+}
