7b0ff8a0c9ae70bb55e65294f14e336c28ba5cd0
Hasan Uddin <abcnizamd@gmail.com>
2026-03-16 16:35:12 +0600

    Create Form from Templates ➕

---
+++ src/Web.Api/Endpoints/Meta/Forms/CreateFromFormTemplate.cs
@@ -0,0 +1,34 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Features.Meta.Forms;
+using Application.Features.Meta.Forms.CreateFromFormTemplate;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Meta.Forms;
+
+internal sealed class CreateFromFormTemplate : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("meta/pages/{pageId}/forms/form-template", async (
+            string pageId,
+            CreateFromFormTemplateRequest request,
+            ICommandHandler<CreateFormFromTemplateCommand, string> handler,
+            CancellationToken ct) =>
+        {
+            var command = new CreateFormFromTemplateCommand(
+                request.TemplateId,
+                pageId,
+                request.Name,
+                request.PrivacyPolicyUrl,
+                request.PrivacyPolicyLinkText,
+                request.FollowUpActionUrl);
+
+            Result<string> result = await handler.Handle(command, ct);
+            return result.Match(id => Results.Created($"meta/forms/{id}", id), CustomResults.Problem);
+        })
+        .WithTags(Tags.Forms)
+        .Produces<FormResponse>(200);
+    }
+}
