43ed7c9dd4269a7d2e0f6f4a801b30ef01c9c98f
Hasan Uddin <abcnizamd@gmail.com>
2026-03-16 16:25:10 +0600

    Form template config

---
+++ src/Infrastructure/Persistence/Configs/FormTemplates/FormTemplateConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Domain.FormQuestions;
+using Domain.FormTemplates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configs.FormTemplates;
+
+internal sealed class FormTemplateConfiguration : IEntityTypeConfiguration<FormTemplate>
+{
+    public void Configure(EntityTypeBuilder<FormTemplate> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.Name).HasMaxLength(200);
+        builder.Property(t => t.Description).HasMaxLength(500);
+        builder.Property(t => t.Questions)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
+                v => JsonSerializer.Deserialize<List<FormQuestion>>(v, (JsonSerializerOptions?)null)!);
+    }
+}
