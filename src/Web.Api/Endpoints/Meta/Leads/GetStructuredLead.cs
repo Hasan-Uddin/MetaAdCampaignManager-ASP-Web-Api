bb727187a21ba9ef5efe63d46902100b44185cd3
Hasan Uddin <abcnizamd@gmail.com>
2026-03-24 19:36:19 +0600

    endpoint

---
+++ src/Web.Api/Endpoints/Meta/Leads/GetStructuredLead.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Features.Meta.Leads.GetStructuredLeads;
+using SharedKernel;
+using Web.Api.Extensions;
+using Web.Api.Infrastructure;
+
+namespace Web.Api.Endpoints.Meta.Leads;
+
+internal sealed class GetStructuredLead : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("meta/forms/{formId}/structured-leads", async (
+            string formId,
+            IQueryHandler<GetStructuredLeadsQuery, List<StructuredLeadResponse>> handler,
+            CancellationToken cancellationToken) =>
+        {
+            Result<List<StructuredLeadResponse>> result = await handler.Handle(new GetStructuredLeadsQuery(formId), cancellationToken);
+            return result.Match(Results.Ok, CustomResults.Problem);
+        }).WithTags(Tags.Leads);
+    }
+}
