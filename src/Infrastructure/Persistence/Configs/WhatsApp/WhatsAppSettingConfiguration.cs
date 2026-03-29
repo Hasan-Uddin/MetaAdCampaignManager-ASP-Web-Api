062a9fe508026a218198c66faf5eb29e79a5034f
Hasan Uddin <abcnizamd@gmail.com>
2026-03-29 09:31:28 +0600

    WhatsApp settings Config

---
+++ src/Infrastructure/Persistence/Configs/WhatsApp/WhatsAppSettingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.WhatsApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configs.WhatsApp;
+
+internal sealed class WhatsAppSettingConfiguration : IEntityTypeConfiguration<WhatsAppSetting>
+{
+    public void Configure(EntityTypeBuilder<WhatsAppSetting> builder)
+    {
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.AccessToken).HasMaxLength(500);
+        builder.Property(s => s.BusinessAccountId).HasMaxLength(50);
+        builder.Property(s => s.PhoneNumberId).HasMaxLength(50);
+        builder.Property(s => s.PhoneNumber).HasMaxLength(20);
+        builder.Property(s => s.WebhookVerifyToken).HasMaxLength(200);
+    }
+}
