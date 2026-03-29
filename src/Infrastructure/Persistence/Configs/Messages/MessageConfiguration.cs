b99d73567b0d2fe1464c53e1a0f4e4f1af4baf1a
Hasan Uddin <abcnizamd@gmail.com>
2026-03-29 09:31:08 +0600

    Messsage Config

---
+++ src/Infrastructure/Persistence/Configs/Messages/MessageConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Messages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configs.Messages;
+
+internal sealed class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(m => m.Id);
+        builder.Property(m => m.WhatsAppMessageId).HasMaxLength(100);
+        builder.Property(m => m.Body).HasMaxLength(4096);
+    }
+}
